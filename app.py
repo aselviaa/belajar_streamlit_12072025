34ab08cef338c683065e79c9ea2b10fa7583c56c
aselviaa <selvianti2105@gmail.com>
2025-07-12 09:03:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")pip install streamlit
-
-streamlit run first_app.py
-
-# Import convention
->>> import streamlit as st
-import streamlit as st
-
 st.write("Hello, *World!* :sunglasses:")
-Copy
-import streamlit as st
 
-st.title("This is a title")
+st.title("This is a title 🤩")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
