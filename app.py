1bec30a95eb8a96fd5b3b91b63ac8ada45d47fe6
aselviaa <selvianti2105@gmail.com>
2025-07-12 09:23:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ st.write("Hello, *World!* :sunglasses:")
 st.title("This is a title 🤩")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-
 st.badge("New")
 st.badge("Success", icon=":material/check:", color="green")
-
 st.markdown(
     ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
 )
