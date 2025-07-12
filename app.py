4b9dae60ba545d81f345a617f96c3a555f150b0b
aselviaa <selvianti2105@gmail.com>
2025-07-12 09:25:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ st.badge("Success", icon=":material/check:", color="green")
 st.markdown(
     ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
 )
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    {
+        "col1": np.random.randn(20),
+        "col2": np.random.randn(20),
+        "col3": np.random.choice(["A", "B", "C"], 20),
+    }
+)
+
+st.area_chart(chart_data, x="col1", y="col2", color="col3")
